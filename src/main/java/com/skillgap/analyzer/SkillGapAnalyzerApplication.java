dc4b6130b7333aa1f3386d39412a0cae3affbbf7
sauravbot19 <tayadesaurav190@gmail.com>
2026-01-31 22:08:18 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/skillgap/analyzer/SkillGapAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.skillgap.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillGapAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillGapAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillgap/analyzer/SkillGapAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillgap.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillGapAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
